4a8d479367ecade9a732ae2451cbfb88129436b2
Rafael Pimentel <rafael.pimentel.ef@outlook.com>
2019-02-04 07:43:35 +0100

    importing the httpclient and using it with mock data and mapping it to a model

--- ClientApp/src/app/services/dateService.ts
+++ ClientApp/src/app/services/dateService.ts
@@ -1,8 +1,36 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Observable";
+import { ServerResponse } from "http";
+import { extend } from "webdriver-js-extender";
 
 @Injectable()
 export class DateService {
+
+  constructor(private http: HttpClient) { }
+
+  private date: ServerDate;
+
   public getCurrentDate(): Date {
+    this.getDateFromServer().subscribe((result) => {
+      this.date.currentDate = result.currentDate;
+      this.date.currentTime = result.currentTime;
+    });
     return new Date();
   }
+
+  public getDateFromServer(): Observable<IServerResponse>{
+    return this.http.get<IServerResponse>("/serverTime");
+  }
+} 
+
+//This would obv be in a models folder 
+class ServerDate implements IServerResponse{
+  currentDate: string;
+  currentTime: string;
+}
+
+interface IServerResponse {
+  currentDate: string;
+  currentTime: string;
 }
