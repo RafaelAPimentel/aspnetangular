e8cc176f4b7c2a580a8f170ef72c471776e88d2b
Rafael Pimentel <rafael.pimentel@USB-MRPIMENTEL.local>
2019-02-17 16:47:57 -0500

    changes

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from './common/common.module'
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -10,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
 import { DateService } from './services/dateService';
+import { SearchComponent } from './search/search.component' 
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { DateService } from './services/dateService';
     NavMenuComponent,
     HomeComponent,
     EducationComponent,
-    SkillsComponent
+    SkillsComponent,
+    SearchComponent
+    
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -27,9 +31,11 @@ import { DateService } from './services/dateService';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'education', component: EducationComponent },
       { path: 'skills', component: SkillsComponent },
-    ])
+      { path: 'search', component: SearchComponent }
+    ]),
+    
   ],
-  providers: [DateService],
+  providers: [DateService,CommonModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
